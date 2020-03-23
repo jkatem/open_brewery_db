2da5492d585c9a2a3f79c2a3de089f900309faeb
jkatem <jenmiao@gmail.com>
2020-03-23 14:31:06 -0700

    Create executable file and environment

---
+++ config/environment.rb
@@ -0,0 +1,8 @@
+require 'pry'
+require 'httparty'
+
+# require_relative '../lib/open_brewery_db/scraper'
+require_relative '../lib/open_brewery_db/api'
+require_relative '../lib/open_brewery_db/cli'
+require_relative '../lib/open_brewery_db/version'
+
